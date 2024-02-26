01823bd35db9171d53cd2883a3a6a22bc4fc1f33
Haoran Li (Evan) <65979806+Haoranli802@users.noreply.github.com>
2024-02-26 15:52:33 +0800

    Update main.java

--- main.java
+++ main.java
@@ -19,3 +19,27 @@
     }
 }
 // Time: O(N) Space: O(1)
+
+// LC 349
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        int[] map = new int[1001];
+        List<Integer> result = new LinkedList<>();
+        for(int i : nums1){
+            map[i] ++;
+        }
+        for(int i : nums2){
+            if(map[i] != 0){
+                if(!result.contains(i)){
+                    result.add(i);
+                }
+            }
+        }  
+        int[] res = new int[result.size()];
+        for(int i = 0; i < result.size(); i++){
+            res[i] = result.get(i);
+        }
+        return res;
+    }
+}
